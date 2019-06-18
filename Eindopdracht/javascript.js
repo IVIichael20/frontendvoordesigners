96b584ddad2062886e2d99f22f7b1153fb7bf0e6
IVIichael20 <50400601+IVIichael20@users.noreply.github.com>
2019-06-18 23:28:05 +0200

    Almost done

--- Eindopdracht/javascript.js
+++ Eindopdracht/javascript.js
@@ -3,10 +3,55 @@
 /*eslint 'no-console':0*/
 
 
+var y;
+
+function movies(Json) {
+    for (y = 0; y < Json.length; y++) {
+
+        var cover = document.createElement('img');
+        var image = Json[y].cover;
+        cover.setAttribute("src", image);
+        cover.setAttribute("class", "sectionImg")
+        document.querySelector('section').appendChild(cover);
+
+    }
+
+    for (y = 0; y < Json.length; y++) {
+
+        cover = document.createElement('img');
+        image = Json[y].cover;
+        cover.setAttribute("src", image);
+        cover.setAttribute("class", "sectionImg")
+        document.querySelector('section').appendChild(cover);
+    }
+
+    for (y = 0; y < Json.length; y++) {
+
+        cover = document.createElement('img');
+        image = Json[y].cover;
+        cover.setAttribute("src", image);
+        cover.setAttribute("class", "sectionImg")
+        document.querySelector('section').appendChild(cover);
+    }
+    var image_remove = document.querySelector('.movie');
+    image_remove.parentNode.removeChild(image_remove);
+
+    document.addEventListener('keydown', function keyCode(e) {
+
+        if (e.keyCode == 27) {
+            var div_toggle = document.getElementById('divje');
+            div_toggle.classList.toggle("divje1");
+
+
+        }
+    })
+}
+
 
 var replacenr = 0;
 
 function buildHTML(Json) {
+
 
     if (replacenr == 0) {
 
@@ -17,52 +62,71 @@ function buildHTML(Json) {
         var h1 = document.createElement('h1');
         document.querySelector('div').appendChild(h1);
         h1.innerHTML = Json[i].title;
-        //   document.getElementsByTagName("h1")[1].setAttribute("class", )
+
 
         var cover = document.createElement('img');
         var image = Json[i].cover;
         cover.setAttribute("src", image)
         document.querySelector('div').appendChild(cover);
 
-        /*   var plot = document.createElement('p');
-           document.querySelector('div').appendChild(plot);
-           plot.innerHTML = Json[i].plot;*/
-
         var release_date = document.createElement('p');
         document.querySelector('div').appendChild(release_date);
         release_date.innerHTML = Json[i].release_date;
-        
+
         var readmore = document.createElement('p');
         document.querySelector('div').appendChild(readmore);
         readmore.innerHTML = "Read more +";
         readmore.setAttribute("class", "readmore");
-        
+
 
         replacenr = replacenr + 1;
-        
-        
+
+        var image_remove = document.querySelector('.movie');
+        image_remove.parentNode.removeChild(image_remove);
+
+
+
+
+
+
 
     } else {
-        
+
         i = Math.floor((Math.random() * 6));
 
         document.getElementsByTagName("h1")[1].innerHTML = Json[i].title;
         document.getElementsByTagName("img")[0].src = Json[i].cover;
         //  document.getElementsByTagName("p")[0].innerHTML = Json[a].plot;
         document.getElementsByTagName("p")[0].innerHTML = Json[i].release_date;
-        
+
         document.querySelector('.readmore').innerHTML = "Read more [+]";
+        document.querySelector('.h3').style.visibility = 'visible';
 
     }
 
-   document.addEventListener('keydown', function keyCode(e) {
+    document.addEventListener('keydown', function keyCode(e) {
         if (e.keyCode == '187') {
-            
-           document.querySelector('.readmore').innerHTML = Json[i].plot;
-            var getal = 0;
+
+            document.querySelector('.readmore').innerHTML = Json[i].plot;
         }
 
     });
+
+    document.addEventListener('keydown', function keyCode(e) {
+
+        if (e.keyCode == 82) {
+            buildHTML(Json);
+        }
+    })
+
+    document.addEventListener('keydown', function keyCode(e) {
+
+        if (e.keyCode == 27) {
+            var div_toggle = document.getElementById('divje');
+            div_toggle.classList.toggle("divje1");
+
+        }
+    })
 
 }
 
@@ -77,9 +141,10 @@ fetch('movies.json')
         document.querySelector('.button').addEventListener('click', function () {
             buildHTML(Json);
         });
-       //movies(Json);
+        document.querySelector('.button1').addEventListener('click', function () {
+            movies(Json);
+        });
 
-        //buildHTML(Json);
 
         console.log(Json);
     })
